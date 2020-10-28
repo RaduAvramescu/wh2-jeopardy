daaa3b2cf5089bfe38dd14af9a7f232c86502868
RaduAvramescu <71271545+RaduAvramescu@users.noreply.github.com>
2020-10-28 17:20:38 +0200

    Updates

---
+++ precache-manifest.b4d2aad679b7052c810e1629d5fe66ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340b1eb703111ca0455f4c799bdcdc2f",
+    "url": "/wh2-jeopardy/index.html"
+  },
+  {
+    "revision": "380f2e69337ad97e4f79",
+    "url": "/wh2-jeopardy/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7f99c343d628a15ba85b",
+    "url": "/wh2-jeopardy/static/css/main.7e5af586.chunk.css"
+  },
+  {
+    "revision": "380f2e69337ad97e4f79",
+    "url": "/wh2-jeopardy/static/js/2.b6a4c79d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/wh2-jeopardy/static/js/2.b6a4c79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f99c343d628a15ba85b",
+    "url": "/wh2-jeopardy/static/js/main.2d94528f.chunk.js"
+  },
+  {
+    "revision": "5ebe2af58c8c5276316a",
+    "url": "/wh2-jeopardy/static/js/runtime-main.228e9ab5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wh2-jeopardy/static/media/logo.5d5d9eef.svg"
+  }
+]);
